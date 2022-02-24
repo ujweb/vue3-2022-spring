2a24bb56db9c34cc92e2b058115917c1291b736e
ujweb <53999525+ujweb@users.noreply.github.com>
2022-02-24 16:50:20 +0800

    checkAccount

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="container" v-if="checkAccount">
+    <h1>測試</h1>
+  </div>
   <router-view v-if="checkAccount" />
 </template>
 
@@ -8,6 +11,26 @@ export default {
     return {
       checkAccount: false,
     };
+  },
+  mounted() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)signinToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    this.$http.defaults.headers.common.Authorization = token;
+    this.checkSignin();
+  },
+  methods: {
+    checkSignin() {
+      const checkUserApi = `${process.env.VUE_APP_API}/api/user/check`;
+      this.$http.post(checkUserApi)
+        .then((response) => {
+          console.log(response);
+          this.checkAccount = true;
+        })
+        .catch((error) => {
+          console.dir(error);
+          alert(error.response.data.message);
+          this.$router.push('/login');
+        });
+    },
   },
 };
 </script>
